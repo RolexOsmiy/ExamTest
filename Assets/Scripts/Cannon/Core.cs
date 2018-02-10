e7b36dddfdc3e8b7e266e996387e15ebf31b1b87
RolexOsmiy <wow.rolex@yandex.by>
2018-02-10 09:25:48 +0300

    123

---
+++ Assets/Scripts/Cannon/Core.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Core : MonoBehaviour
+{
+	float distance = 0f;
+	GameObject parent = null;
+	public float speed = 10f;
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.Translate(Vector3.forward * speed * Time.deltaTime);
+		if (Vector3.Distance(parent.transform.position, transform.position) >= distance) {
+			Destroy (this.gameObject);
+		}
+	}
+
+	public void SetVars(float _distance, GameObject _parent)
+	{
+		distance = _distance;
+		parent = _parent;
+	}
+
+	void OnCollisionEnter(Collision coll)
+	{
+		if (coll.gameObject.tag == "Player") {
+			Destroy (this.gameObject);
+			Debug.Log ("Hit Player!");
+		}
+	}
+}
+
---
+++ Assets/Scripts/Player/Controller.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class Controller : MonoBehaviour {
+
+	Camera cam;
+	PlayerMotor motor;
+	public LayerMask movementMask;
+	GameObject target;
+
+	void Start () {
+		cam = Camera.main;
+		motor = GetComponent<PlayerMotor> ();
+	}	
+
+	void Update ()
+	{
+		if (Input.GetMouseButton(1)) {
+			Ray ray = cam.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast(ray, out hit, 100, movementMask)) {
+				motor.MoveToPoint (hit.point);
+			}
+		}
+
+		if (Input.GetMouseButtonDown(0)) {
+			Ray ray = cam.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast(ray, out hit, 100)) 
+			{
+				if (hit.collider.gameObject.tag == "Destroy") {
+					target = hit.collider.gameObject;
+				}
+				if (target) {
+					motor.MoveToPoint (hit.point);
+				}
+			}
+
+		}
+
+		if (target) {
+			if (Vector3.Distance(target.transform.position,transform.position)<= 1) {
+				
+			}
+
+		}
+	}
+
+	void OnCollisionStay(Collision coll)
+	{
+		if (target) {
+			if (coll.gameObject.name == target.gameObject.name) {			
+				StartCoroutine(Attack());
+			}
+		}
+	}
+
+	IEnumerator Attack()
+	{
+		yield return new WaitForSeconds(0.5f);
+		Destroy(target);
+	}
+}
